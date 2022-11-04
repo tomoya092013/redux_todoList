c12b7c88f91c7c6c398901fe2b29da3b3d1c5905
tomoya <cj.0s0ttw.nsm4@gmail.com>
2022-11-05 07:37:43 +0900

    リダックスでtodo機能作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const [name, setName] = useState("");
+  const [complete, setComplete] = useState(false);
+
+  const lists = useSelector((state) => state.lists);
+  const dispatch = useDispatch();
+  const inputText = (e) => {
+    setName(e.target.value);
+  };
+  // console.log(lists);
+
+  const doneList = (name) => {
+    dispatch({ type: "DONE_LIST", payload: name });
+  };
+  const deleteList = (name) => {
+    dispatch({ type: "DELETE_LIST", payload: name });
+  };
+  const addList = () => {
+    if (!name) return;
+    setComplete(false);
+    dispatch({
+      type: "ADD_LIST",
+      payload: {
+        name,
+        complete,
+      },
+    });
+    setName("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ReduxでTodoリスト作成！</h1>
+      <input type="text" value={name} onChange={inputText} />
+      <button onClick={addList}>追加</button>
+      <h2>未完了のTodoリスト</h2>
+      <ul>
+        {lists
+          .filter((list) => list.complete === false)
+          .map((list, index) => (
+            <li key={index}>
+              {list.name}
+              <button onClick={() => doneList(list.name)}>完了</button>
+              <button onClick={() => deleteList(list.name)}>削除</button>
+            </li>
+          ))}
+      </ul>
+      <h2>完了したTodoリスト</h2>
+      <ul>
+        {lists
+          .filter((list) => list.complete === true)
+          .map((list, index) => (
+            <li key={index}>{list.name}</li>
+          ))}
+      </ul>
     </div>
   );
 }
